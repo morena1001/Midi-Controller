a42e4405e5eb1e0acd98684830257651a1e43b18
morena1001 <josueflores15.jf@gmail.com>
2025-07-06 19:14:47 -0500

    Added initial poll of potentiometers to match it to the software

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -153,33 +153,29 @@ int main(void)
   /* USER CODE BEGIN 2 */
   Queue_Init (DEFAULT_QUEUE_SIZE);
 
-//	for (uint8_t i = 0; i < 16; i++)	D_sum += ADC_Convert_Rank1 ();
-//
-//	D_current = ((D_sum >> 4) * 127) / 4095;
-//	D_sum = 0;
-//
-//	if (D_current < D_previous - 3 || D_current > D_previous + 3) {
-//		D_previous = D_current;
-//		D_vol_message [3] = D_current;
-//
-//	//		while (USBD_MIDI_GetState (&hUsbDeviceFS) != MIDI_IDLE) {}
-//		USBD_MIDI_SendPackets (&hUsbDeviceFS, D_vol_message, 4);
-//	//		Enqueue (D_vol_message);
-//	}
-//
-//	for (uint8_t i = 0; i < 16; i++)	P_sum += ADC_Convert_Rank2 ();
-//
-//	P_current = ((P_sum >> 4) * 127) / 4095;
-//	P_sum = 0;
-//
-//	if (P_current < P_previous - 3 || P_current > P_previous + 3) {
-//		P_previous = P_current;
-//		D_vol_message [3] = P_current;
-//
-//	//		while (USBD_MIDI_GetState (&hUsbDeviceFS) != MIDI_IDLE) {}
-//		USBD_MIDI_SendPackets (&hUsbDeviceFS, P_vol_message, 4);
-//	//		Enqueue (P_vol_message);
-//	}
+	for (uint8_t i = 0; i < 16; i++)	D_sum += ADC_Convert_Rank1 ();
+
+	D_current = ((D_sum >> 4) * 127) / 4095;
+	D_sum = 0;
+
+	D_previous = D_current;
+	D_vol_message [3] = D_current;
+
+//		while (USBD_MIDI_GetState (&hUsbDeviceFS) != MIDI_IDLE) {}
+	USBD_MIDI_SendPackets (&hUsbDeviceFS, D_vol_message, 4);
+//		Enqueue (D_vol_message);
+
+	for (uint8_t i = 0; i < 16; i++)	P_sum += ADC_Convert_Rank2 ();
+
+	P_current = ((P_sum >> 4) * 127) / 4095;
+	P_sum = 0;
+
+	P_previous = P_current;
+	P_vol_message [3] = P_current;
+
+//		while (USBD_MIDI_GetState (&hUsbDeviceFS) != MIDI_IDLE) {}
+	USBD_MIDI_SendPackets (&hUsbDeviceFS, P_vol_message, 4);
+//		Enqueue (P_vol_message);
 
   HAL_NVIC_SetPriority (TIM2_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ (TIM2_IRQn);
